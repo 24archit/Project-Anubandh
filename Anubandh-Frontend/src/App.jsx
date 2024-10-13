baf63027476ceafd6d26ef58cec16622c6249f8f
Archit Mishra <architmishra016@gmail.com>
2024-10-13 20:02:45 +0530

    Updated the section heading

---
+++ Anubandh-Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import JobPortalHomePage from "./pages/JobPortal/JobPortalHomePage"
+import NavBar from "./components/Navbar"
+function App() {
+ 
+  return (
+    <>
+     <NavBar/>
+     <JobPortalHomePage/>
+    </>
+  )
+}
+
+export default App
