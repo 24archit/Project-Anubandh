296657b41819d140ac84207d23d9908ee5c3af0b
Archit Mishra <architmishra016@gmail.com>
2024-12-22 03:24:58 +0530

    Added

--- Anubandh-Frontend/src/pages/DonationPage.jsx
+++ Anubandh-Frontend/src/pages/DonationPage.jsx
@@ -1,9 +1,11 @@
 import DonationComponent from "../components/DonationComponent";
 import WallOfDonors from "../components/WallOfDonors";
 import TypewriterComponent from "../components/TypewriterComponent";
+import NavBar from "../components/Navbar";
 export default function HomePage() {
   return (
     <>
+      <NavBar/>
       <TypewriterComponent text="Empower Future Leaders,', 'Your Generosity Fuels Change."/>
       <WallOfDonors />
       <DonationComponent />
