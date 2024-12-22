6b3546867845945bccc4b4c9d0a27777a72f8563
Archit Mishra <architmishra016@gmail.com>
2024-12-22 17:58:49 +0530

    Added

--- Anubandh-Frontend/src/pages/JobPortal/JPHomePage.jsx
+++ Anubandh-Frontend/src/pages/JobPortal/JPHomePage.jsx
@@ -1,12 +1,12 @@
 import Banner from "../../components/Banner";
 import TypewriterComponent from "../../components/TypewriterComponent";
 import SearchForm from "../../components/SearchForm";
-import NavBar from "../../components/Navbar";
+import NavBarJob from "../../components/NavbarJob";
 function JobPortalPage() {
   return (
     <>
-      <NavBar />
-      <TypewriterComponent text="Your Next Career Move Starts Here,', 'With Your Alumni Connections." />
+      <NavBarJob />
+      <TypewriterComponent text="Your Next Career Move Starts Here, With Your Alumni Connections." />
       <SearchForm />
       <Banner />
     </>
